099102629d2ad9b1d682804c4e9a6b20beee6690
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-03-19 21:55:46 +0300

    feat: live search task

--- 5-live-search/src/request-to-html-string.ts
+++ 5-live-search/src/request-to-html-string.ts
@@ -1,5 +1,23 @@
 import { Card } from "./card.interface";
 
+export function requestToHtmlString(cards: Card[]): string {
+    const cardsToHtml = cards.map(createCard);
+    const groupedCards = grouping(cardsToHtml, 3).map(createRow);
+
+    return groupedCards.join('');
+}
+
+function grouping<T>(array: T[], groupSize: number): Array<T[]> {
+    const groupsLength = Math.ceil(array.length / groupSize);
+
+    return Array.from({length: groupsLength}).map((_, index) => {
+        const startPosition = index * groupSize;
+        const endPosition = startPosition + groupSize;
+
+        return array.slice(startPosition, endPosition);
+    })
+}
+
 function createCard({name, description, owner}: Card): string {
     return `
     <div class="col-sm-6 col-md-4">
