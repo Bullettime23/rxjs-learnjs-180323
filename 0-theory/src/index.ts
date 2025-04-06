9cf37f736303182fc498192770c6f8bc3b1fec0f
Bullettime23 <letsgofrontend@gmail.com>
2025-04-06 12:56:32 +0500

    Learn HOO

--- 0-theory/src/index.ts
+++ 0-theory/src/index.ts
@@ -1,4 +1,4 @@
-import { Observable, Subscriber, Subscription, defer, fromEvent, interval, take } from 'rxjs';
+import { Observable, Subscriber, Subscription, concat, concatAll, concatMap, defer, fromEvent, fromEventPattern, interval, map, of, take, tap, timer } from 'rxjs';
 import '../../assets/css/style.css';
 import { terminalLog } from '../../utils/log-in-terminal';
 
@@ -142,3 +142,50 @@ import { terminalLog } from '../../utils/log-in-terminal';
 // def.subscribe(terminalLog);
 // def.subscribe(terminalLog);
 // def.subscribe(terminalLog);
+
+// function addListener(handler: EventListener) {
+//     document.addEventListener('click', handler);
+//     terminalLog('LISTENER ADDED');
+// }
+
+// function removeListener(handler: EventListener) {
+//     document.removeEventListener('click', handler);
+//     terminalLog('LISTENER REMOVED');
+// }
+
+// fromEventPattern(addListener, removeListener).pipe(take(5)).subscribe((event: any)=> {
+// 	console.log(event)
+// });
+
+// HOO
+
+//concatMap = (map ev -> Interval) + concat
+
+// const clicks = fromEvent(document, 'click');
+// const result = clicks.pipe(
+//   concatMap((ev: Event) => interval(1000).pipe(take(4)))
+// );
+// result.subscribe(x => terminalLog(x));
+
+// Results in the following:
+// (results are not concurrent)
+// For every click on the "document" it will emit values 0 to 3 spaced
+// on a 1000ms interval
+// one click = 1000ms-> 0 -1000ms-> 1 -1000ms-> 2 -1000ms-> 3
+
+// const clicks$ = fromEvent(document, 'click');
+
+// const higherOrder$ = clicks$.pipe(map(e=> interval(1000).pipe(take(3))));
+
+// const firstOrder$ = higherOrder$.pipe(concatAll());
+// firstOrder$.subscribe(x => terminalLog(x));
+
+/**
+ * mergeMap работает, как map -> obs, потом merge в своём коллбэке. Возвращает плоские обсерваблы
+ * mergeAll вызывается в пайпе HOO и возвращает плоский обсервабл
+ * 
+ * concatAll - последовательно подписывается и отписывается
+ * mergeAll - значения вперемешку
+ * swithcAll - подписывается по очереди, но бросает предыдущий, когда эмитит следующий obs
+ * exhaustAll - подписывается по очереди, но игнорирует значения следующего obs, пока текущий не закончится
+ */
